ca8a5b720cd6a5dcb8d28d6a9f507797fdbae599
e <s>
2020-04-18 15:01:09 -0700

    ah

--- gconfig.sh
+++ gconfig.sh
@@ -1,3 +1,29 @@
+ready=0;
+
+while ! (($ready))
+do
+    read -p "Do you share this computer (y/n)? " global
+    if [ $global == "y" ]; then
+        g="--global"
+        ready=1
+    elif [ $global == "n" ]; then
+        g=""
+        ready=1
+    fi
+done
+
 if [ -z "$(command -v git)" ]; then    
     sudo apt install git
 fi
+
+read -p "Username: " username
+git config $g user.name "$username"
+read -p "Email: "  email
+git config $g user.email "$email"
+
+if [ -z "$(cat ~/.ssh/id_rsa.pub)" ]; then
+    echo 'USE DEFAULTS: just click enter twice'
+    ssh-keygen
+fi
+
+
